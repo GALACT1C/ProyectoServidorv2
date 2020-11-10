b55b85de04f8f68c9ba0ac44ba32caf66ec9dcf8
Carlos <carlostamarit18@gmail.com>
2020-11-10 11:27:00 +0100

    Heredacion casi terminada (faltan los assets)

--- src/Controller/ProductosController.php
+++ src/Controller/ProductosController.php
@@ -10,21 +10,48 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 class ProductosController extends AbstractController
 {
     /**
-     * @Route("/productos", name="productos")
+     * @Route("/productos/{productos}", name="productos")
      */
-    public function index(SessionInterface $session)
+    public function index($productos=1, SessionInterface $session)
     {
         $usuario = $session->get('usuario');
-        return $this->render('/productos.html.twig', [
-            'loginpage'=>$this->generateUrl('login'),
-            'indexpage'=>$this->generateUrl('inicio'),
-            'aboutpage'=>$this->generateUrl('nosotros'),
-            'productpage'=>$this->generateUrl('productos'),
-            'product2page'=>$this->generateUrl('productos2'),
-            'product3page'=>$this->generateUrl('productos3'),
-            'contactapage'=>$this->generateUrl('contacta'),
-            'servicespage'=>$this->generateUrl('servicios'),
-            'usuario' => strlen($usuario)>0?''.$usuario:'',
-        ]);
+    
+        if($productos == 2){
+            return $this->render('/productos2.html.twig', [
+                'loginpage'=>$this->generateUrl('login'),
+                'indexpage'=>$this->generateUrl('inicio'),
+                'aboutpage'=>$this->generateUrl('nosotros'),
+                'productpage'=>$this->generateUrl('productos'),
+                'product2page'=>$this->generateUrl('productos2'),
+                'product3page'=>$this->generateUrl('productos3'),
+                'contactapage'=>$this->generateUrl('contacta'),
+                'servicespage'=>$this->generateUrl('servicios'),
+                'usuario' => strlen($usuario)>0?''.$usuario:'',
+            ]);
+        }else if($productos == 3){
+            return $this->render('/productos3.html.twig', [
+                'loginpage'=>$this->generateUrl('login'),
+                'indexpage'=>$this->generateUrl('inicio'),
+                'aboutpage'=>$this->generateUrl('nosotros'),
+                'productpage'=>$this->generateUrl('productos'),
+                'product2page'=>$this->generateUrl('productos2'),
+                'product3page'=>$this->generateUrl('productos3'),
+                'contactapage'=>$this->generateUrl('contacta'),
+                'servicespage'=>$this->generateUrl('servicios'),
+                'usuario' => strlen($usuario)>0?''.$usuario:'',
+            ]);
+        }else{
+            return $this->render('/productos.html.twig', [
+                'loginpage'=>$this->generateUrl('login'),
+                'indexpage'=>$this->generateUrl('inicio'),
+                'aboutpage'=>$this->generateUrl('nosotros'),
+                'productpage'=>$this->generateUrl('productos'),
+                'product2page'=>$this->generateUrl('productos2'),
+                'product3page'=>$this->generateUrl('productos3'),
+                'contactapage'=>$this->generateUrl('contacta'),
+                'servicespage'=>$this->generateUrl('servicios'),
+                'usuario' => strlen($usuario)>0?''.$usuario:'',
+            ]);
+        }
     }
 }
